683494e88d2db0c26272acde375a884a0d9febdd
mohlade <100697842+mohlade@users.noreply.github.com>
2022-05-09 21:24:28 +0300

    Initial commit and fixed css properties

---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Strange_evil',
+      username: 'Omolade',
+      email: 'victor_omolade@yahoo.com'
+    },
+    {
+      id: 2,
+      name: 'Da_Priest',
+      username: 'Da Priest',
+      email: 'Dapriest@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Just_Donald',
+      username: 'Donlad',
+      email: 'justdonald@yahoo.com'
+    },
+    {
+      id: 4,
+      name: 'Willie Willie',
+      username: 'willie',
+      email: 'willie-willie@yahoo.com'
+    },
+    {
+      id: 5,
+      name: 'T.G.M.O',
+      username: 'Tocho',
+      email: 'Tgmo@yahoo.com'
+    },
+    {
+      id: 6,
+      name: 'Just_Jess',
+      username: 'Jess',
+      email: 'justjess@yahoo.com'
+    },
+    {
+      id: 7,
+      name: 'AdeWithTheWave',
+      username: 'Ade',
+      email: 'Adewiththewave@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'Lad_ovvy',
+      username: 'Ovvy',
+      email: 'ladyovvy@yahoo.com'
+    },
+    {
+      id: 9,
+      name: 'Charlie Willie',
+      username: 'Charlie',
+      email: 'Useless.rabbit@yahoo.com'
+    },
+    {
+      id: 10,
+      name: 'Trent Kwaku',
+      username: 'Kwaku',
+      email: 'Very.slow@yahoo.com'
+    },
+    {
+        id: 11,
+        name: 'Snow Ball',
+        username: 'Snow',
+        email: 'cute.white.rabbit@yahoo.com'
+    },
+    {
+        id: 12,
+        name: 'Oreo',
+        username: 'Biscuit',
+        email: 'oreosbiscuit@yahoo.com',
+
+    }
+  ];
+  
